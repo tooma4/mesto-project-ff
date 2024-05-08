4fa431bbefc92f57a09d5046404fee872f077e6c
Roman Starshinov <romanstarshinovdev@gmail.com>
2024-05-08 23:07:37 +0300

    Исправил все замечания ревьюера, после первой проверки

--- scripts/index.js
+++ scripts/index.js
@@ -8,26 +8,35 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 //место куда будут создаваться карточки
 
-const placeList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCards(initialCard, removeCard) {
-  //Копирую шаблон, задаю значения и вставляю на страницу
+function createCard(dataCard, removeCard) {
+  //Копирую шаблон, нахожу элементы и записываю в переменные
 
-  for (const card of initialCard) {
-    const newCard = cardTemplate.cloneNode(true);
-
-    newCard.querySelector('.card__image').src = card.link;
-    newCard.querySelector('.card__image').alt = card.name;
-    newCard.querySelector('h2').textContent = card.name;
-    placeList.append(newCard);
-  }
+  let newCard = cardTemplate.cloneNode(true);
+  let cardImageLink = newCard.querySelector('.card__image');
+  let cardImageAlt = newCard.querySelector('.card__image');
+  let cardTitle = newCard.querySelector('.card__title');
 
   //нахожу кнопку удаления карточки и вешаю обработчик
 
-  const iconDelete = document.querySelectorAll('.card__delete-button');
-  iconDelete[addEventListener('click', removeCard)];
+  let iconDelete = newCard.querySelector('.card__delete-button');
+  iconDelete.addEventListener('click', removeCard);
+
+  // задаю значения и возвращаю карточку
+
+  cardImageLink.src = dataCard.link;
+  cardImageAlt.alt = dataCard.name;
+  cardTitle.textContent = dataCard.name;
+
+  return newCard;
+}
+// функция добавления карточки
+
+function addCard(card) {
+  cardsContainer.append(card);
 }
 
 // @todo: Функция удаления карточки
@@ -36,9 +45,14 @@ function removeCard(evt) {
   //передаю ближайший родительский элемент и удаляю
 
   const card = evt.target.closest('.card');
+  console.log('удаление');
   card.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-createCards(initialCards, removeCard);
+//Перебираю массив на каждом элементе и передаю элемент массива в функцию, затем выводится 6 карточек на страницу
+
+initialCards.forEach((dataCard) => {
+  addCard(createCard(dataCard, removeCard));
+});
